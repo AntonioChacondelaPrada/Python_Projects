93822807833b6803916c490868f0f3bee2a8d39e
AntonioChacondelaPrada <a.chacondelaprada@gmail.com>
2023-11-28 22:52:37 +0100

    progress of App1. Uploaded to Github1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip()
     match user_action:
         case 'add':
@@ -11,6 +11,14 @@ while True:
             for item in todos:
                 item = item.capitalize()
                 print(item)
+
+        case "edit":
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            new_todo = input("write the mew tasks: ")
+            todos[number] = new_todo
+            print(todos)
+
         case "exit":
             break
         case _:
